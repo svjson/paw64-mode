db9a1ba45568474c1fc3c6cd39244f4ffc1bc24d
svjson <johansson.sven@gmail.com>
2022-01-12 20:59:36 +0100

    Resolve includes during flycheck using the original dir root

--- paw64-mode.el
+++ paw64-mode.el
@@ -255,7 +255,7 @@
 
 (flycheck-define-checker 64tass
   "A 6502 assembly syntax checker using 64tass."
-  :command ("64tass" source "--no-output")
+  :command ("64tass" source "--no-output" "-I" (eval (file-name-directory buffer-file-name)))
   :error-patterns
   ((error line-start (file-name) ":" line ":" column ": " (optional "fatal ") "error: " (message) line-end))
   :modes paw64-mode
